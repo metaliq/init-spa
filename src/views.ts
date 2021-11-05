6e6528d32844c4dac38647e75235551a46e321fc
tstewart <tstewart@klaudhaus.com>
2021-11-05 14:18:54 +1100

    Upgrade MQ version Improve Hello World styling

--- src/views.ts
+++ src/views.ts
@@ -4,18 +4,21 @@ import { up } from "@metaliq/up"
 import { HelloWorld } from "./types"
 
 /**
- * A view template function, using Google's `lit` package and the built-in power of JS template literals
+ * A view template function, using Google's `lit` package and the built-in power of JS template literals.
  */
 export const greetMeView = (model: HelloWorld) => html`
-  <div>Enter your name (or "World" if you prefer)</div>
-  <input type="text" @input=${up(setName, model)}/>
+  <label class="mq-label">
+    Enter your name (or "World" if you prefer)
+    <input class="mq-input" type="text" 
+      @input=${up(setName, model)}>
+  </label>
   ${model.name ? html`
-    <div>Hello, ${model.name}!</div>
+    <p>Hello, ${model.name}!</p>
   ` : ""}
 `
 
 /**
- * A process that accepts the model and updates its name
+ * A process that sets the `name` value on the  model from the event target.
  */
 function setName (model: HelloWorld, event: Event) {
   model.name = (<HTMLInputElement>event.target).value
